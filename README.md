0968529b48465cd4b1e900aa761ec5c311b15e55
@flank.silva <flank.silva@cenariocapital.com.br>
2023-05-29 22:12:56 -0300

    feat - config tests

---
+++ README.md
@@ -0,0 +1,11 @@
+### Criar container
+docker compose up -d 
+
+### Criar tabelas
+npx prisma migrate dev
+
+### Rodar servidor
+npm run dev
+
+### Exibir tabelas
+npx prisma studio
---
+++ src/http/controllers/register.ts
@@ -0,0 +1,34 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exist-error'
+
+export const register = async (req: FastifyRequest, reply: FastifyReply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  try {
+    const prismaUserRepository = new PrismaUsersRepository()
+    const registerUserCase = new RegisterUseCase(prismaUserRepository)
+
+    await registerUserCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    throw error
+  }
+
+  return reply.status(201).send()
+}
